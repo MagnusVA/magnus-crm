83a1df97467148781a7892dc882113e5576e6c04
nimbus <claudio.vasquez@unah.hn>
2026-04-20 19:30:56 -0600

    remove react scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,13 +41,13 @@ export default function RootLayout({
 				"font-sans",
 			)}
 		>
-			<head>
+			{/* <head>
 				<Script
 					src="//unpkg.com/react-scan/dist/auto.global.js"
 					crossOrigin="anonymous"
 					strategy="beforeInteractive"
 				/>
-			</head>
+			</head> */}
 			<body className="min-h-full flex flex-col">
 				<Suspense>
 					<ConvexClientProvider>
