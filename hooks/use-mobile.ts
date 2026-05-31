bc200d58d0c33ce8d073468f871c63f0cc779d40
nimbus <claudio.vasquez@unah.hn>
2026-05-31 13:25:42 -0600

    Use mobile sidebar below desktop widths
    - Add a sidebar-specific mobile breakpoint at 1280px
    - Keep the fixed sidebar hidden until xl layouts

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,19 +1,29 @@
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
+export const SIDEBAR_MOBILE_BREAKPOINT = 1280
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+function useMediaBelow(breakpoint: number) {
+  const [isBelow, setIsBelow] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const mql = window.matchMedia(`(max-width: ${breakpoint - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      setIsBelow(window.innerWidth < breakpoint)
     }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    setIsBelow(window.innerWidth < breakpoint)
     return () => mql.removeEventListener("change", onChange)
-  }, [])
+  }, [breakpoint])
 
-  return !!isMobile
+  return !!isBelow
+}
+
+export function useIsMobile() {
+  return useMediaBelow(MOBILE_BREAKPOINT)
+}
+
+/** Sidebar collapses to a sheet below this width (1280px). */
+export function useIsSidebarMobile() {
+  return useMediaBelow(SIDEBAR_MOBILE_BREAKPOINT)
 }
